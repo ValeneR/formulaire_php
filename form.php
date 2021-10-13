0c2087067048ca90c9520397f8e824b424f7da82
Valene Rassat <valene.rassat@gmail.com>
2021-10-13 11:54:22 +0200

    form

---
+++ form.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <form  action="thanks.php"  method="get">
+    <div>
+      <label  for="name">Nom :</label>
+      <input  type="text"  id="name"  name="user_name">
+    </div>
+    <div>
+      <label  for="firstname">Prénom :</label>
+      <input  type="text"  id="firstname"  name="user_firstname">
+    </div>
+    <div>
+      <label  for="courriel">Courriel :</label>
+      <input  type="email"  id="courriel"  name="user_email">
+    </div>
+    <div>
+      <label  for="phone_number">Téléphone :</label>
+      <input  type="tel"  id="phone_number"  name="user_phone">
+    </div>
+    <div>
+      <label for="subject">Sujet :</label>
+      <select name="subject" size=1>
+        <option>Commande
+        <option>Livraison
+        <option>Payement
+        <option>Autre
+      </select>
+    </div>
+    <div>
+      <label  for="message">Message :</label>
+      <textarea  id="message"  name="user_message"></textarea>
+    </div>
+    <div  class="button">
+      <button  type="submit">Envoyer votre message</button>
+    </div>
+  </form>
+</html>
