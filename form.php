e90109db64f7cdcd0c33480f747b9753ccbb624b
Valene Rassat <valene.rassat@gmail.com>
2021-10-14 11:18:25 +0200

    quete 2

--- form.php
+++ form.php
@@ -3,23 +3,23 @@
   <form  action="thanks.php"  method="post">
     <div>
       <label  for="name">Nom :</label>
-      <input  type="text"  id="name"  name="user_name">
+      <input  type="text"  id="name"  name="name" required>
     </div>
     <div>
       <label  for="firstname">Prénom :</label>
-      <input  type="text"  id="firstname"  name="user_firstname">
+      <input  type="text"  id="firstname"  name="firstname" required>
     </div>
     <div>
-      <label  for="courriel">Courriel :</label>
-      <input  type="email"  id="courriel"  name="user_email">
+      <label  for="email">Courriel :</label>
+      <input  type="email"  id="email"  name="email" required>
     </div>
     <div>
       <label  for="phone_number">Téléphone :</label>
-      <input  type="tel"  id="phone_number"  name="user_phone">
+      <input  type="tel"  id="phone_number"  name="phone_number" required>
     </div>
     <div>
       <label for="subject">Sujet :</label>
-      <select name="subject" size=1>
+      <select name="subject" size=1 required>
         <option>Commande
         <option>Livraison
         <option>Payement
@@ -28,7 +28,7 @@
     </div>
     <div>
       <label  for="message">Message :</label>
-      <textarea  id="message"  name="user_message"></textarea>
+      <textarea  id="message"  name="message" required></textarea>
     </div>
     <div  class="button">
       <button  type="submit">Envoyer votre message</button>
--- thanks.php
+++ thanks.php
@@ -1,5 +1,18 @@
 <?php
-    echo "Merci " . $_GET[ 'user_name'] . " " . $_GET[ 'user_firstname'] . " de nous avoir contacté à propos de '" . $_GET['subject'] . "'. " . 
-  "Un de nos conseiller vous contactera soit à l'adresse " . $_GET[ 'user_email'] .  " ou par téléphone au " . $_GET['user_phone'] . " dans les plus brefs délais pour traiter votre demande :" . 
-  $_GET['user_message'];
-?>
+
+$retour = "Merci " . $_GET[ 'name'] . " " . $_GET[ 'firstname'] . " de nous avoir contacté à propos de '" . $_GET['subject'] . "'. " . 
+"Un de nos conseiller vous contactera soit à l'adresse " . $_GET[ 'email'] .  " ou par téléphone au " . $_GET['phone_number'] . " dans les plus brefs délais pour traiter votre demande :" . 
+$_GET['message'];
+$nameErr = $firstnameErr = $emailErr = $phone_numberErr = $subjectErr = $messageErr = "";
+$name = $firstname = $email = $phone_number = $subject = $message = "";
+
+if (empty($_POST[])) {
+    $nameErr = "Ce champ est requis";
+  } else {
+    $name = test_input($_POST[]);
+  }
+
+$email = test_input(($_POST['email']));
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $emailErr = "Adresse mail invalide!"
+}
